5b56d54b418fdcff909bd3e0906516da9a4f282e
unknown <v-weiqi@XIANWIN8-V03.redmond.corp.microsoft.com>
2013-01-08 22:38:21 -0800

    push MvcApplication1 operation S2

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to kick-start your ASP.NET MVC application #3. Success build 1";
+            ViewBag.Message = "Modify this template to kick-start your ASP.NET MVC application #3. Success build 2";
 
             return View();
         }
